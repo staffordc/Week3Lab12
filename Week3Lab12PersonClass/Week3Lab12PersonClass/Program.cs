409866d967e0cb601efd9c687af1e4785a54e422
staffordc <stafford.carh@gmail.com>
2018-07-31 13:38:33 -0400

    initial

---
+++ Week3Lab12PersonClass/Week3Lab12PersonClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Lab12PersonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person(name:string, address: string )
+        }
+    }
+}
